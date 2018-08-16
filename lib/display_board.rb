bb00d1de0935462055d13665e7c22351a21b24ab
Amil Masic <amil.masic@gmail.com>
2018-08-16 16:34:15 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,17 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-display_row1 = ["   ", "|", "   ", "|", "   "]
-display_row2 = "-----------"
+empty = "   "
+pipe = "|"
+dashes ="-----------"
+row1 = print empty, pipe, empty, pipe, empty
+row2= print dashes
+
 def display_board
   puts "A Tic Tac Toe Board"
-  puts display_row1
-  puts  display_row2
+  puts row1
+  puts row2
+  puts row1
+  puts row2
+  puts row1
 
 end
+
