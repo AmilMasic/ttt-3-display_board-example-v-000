4cbc0c1b925f7a9cc2f442737312f56ab8d54f98
Amil Masic <amil.masic@gmail.com>
2018-08-16 16:39:05 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,15 +2,12 @@
 empty = "   "
 pipe = "|"
 dashes ="-----------"
-row1 = empty, pipe, empty, pipe, empty
-row2 = dashes
 
 def display_board
   puts "A Tic Tac Toe Board"
-  puts row1
-  puts row2
-  puts row1
-  puts row2
-  puts row1
-
+  print empty, pipe, empty, pipe, empty
+  print dashes
+  print empty, pipe, empty, pipe, empty
+  print dashes
+  print empty, pipe, empty, pipe, empty
 end
